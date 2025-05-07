7a35a7728f18419508f46e57ffeaf0ff94779888
Ítalo Lima Verde <20221101110079@ifrn.local>
2025-05-07 17:47:12 -0300

    aula01

---
+++ primeiro projeto/src/Componente.jsx
@@ -0,0 +1,15 @@
+function Componente() {
+    const meu_nome = "italo"
+    return (
+        <>
+            <div>
+                <h1>Olá, mundo!</h1>
+            </div>
+            <div>
+                <p>Meu nome é {meu_nome}</p>
+            </div>
+        </>
+    )
+}
+
+export default Componente
