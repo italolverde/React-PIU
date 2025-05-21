4f66f878c6eb651f9aaa016b0fb1a9cd3e9d1800
italolverde <italolverde3@gmail.com>
2025-05-21 11:14:52 -0300

    parte jogo da velha

--- testes/moving_object/src/App.jsx
+++ testes/moving_object/src/App.jsx
@@ -3,8 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Screen from './componentes/Screen'
-import Joystick from './componentes/Joystick'
-import DraggableBox from './componentes/DraggableBox'
+
 
 function App() {
   const [count, setCount] = useState(0)
--- testes/moving_object/src/componentes/Screen.jsx
+++ testes/moving_object/src/componentes/Screen.jsx
@@ -1,28 +1,34 @@
 import "./Screen.css"
 import { useState } from "react"
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
 
 function Screen() {
 
-    const [position,setPosition] = useState( { top:100,left:100 } )
-    const handleKeyDown = (e) => {
-        console.log('aa')
-        setPosition((prev) => {
-            const step = 2
-            switch(e.key) {
-                case "w":
-                    return {...prev, top: prev.top - step}
-                default:
-                    return prev
-                }
-        })
-    } 
+    const [tabuleiro, setTabuleiro] = useState(Array(9).fill(null))
+    const [vezdoX, setVezdoX] = useState(true)
 
-    window.addEventListener("keydown", handleKeyDown);
+    function handleclick(index) {
+        const simbolo = vezdoX ? 'X' : 'O';
+
+        let modtabuleiro = [...tabuleiro]
+        if(modtabuleiro[index] == null) { 
+            modtabuleiro[index] = simbolo 
+            setTabuleiro(modtabuleiro)
+            setVezdoX(!vezdoX)
+        }
+    }
 
     return (
         <div id="tela">
-            <div style={{top:position.top,left:position.left,position: 'relative'}} id="objct"></div>
+            <div className="peca" onClick={() => handleclick(0)}>{tabuleiro[0]}</div>
+            <div className="peca" onClick={() => handleclick(1)}>{tabuleiro[1]}</div>
+            <div className="peca" onClick={() => handleclick(2)}>{tabuleiro[2]}</div>
+            <div className="peca" onClick={() => handleclick(3)}>{tabuleiro[3]}</div>
+            <div className="peca" onClick={() => handleclick(4)}>{tabuleiro[4]}</div>
+            <div className="peca" onClick={() => handleclick(5)}>{tabuleiro[5]}</div>
+            <div className="peca" onClick={() => handleclick(6)}>{tabuleiro[6]}</div>
+            <div className="peca" onClick={() => handleclick(7)}>{tabuleiro[7]}</div>
+            <div className="peca" onClick={() => handleclick(8)}>{tabuleiro[8]}</div>
         </div>
     )
     }
